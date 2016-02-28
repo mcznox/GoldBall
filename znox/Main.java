98845f1b530b52b2a3f5570fa8eeea00bfc55557
zNoX_ <tybeatscode@gmail.com>
2016-02-28 11:48:54 -0300

    Add annotation in register events

---
+++ znox/Main.java
@@ -0,0 +1,38 @@
+package znox;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import znox.Oversee.Oversee;
+import znox.Type.Elements.Game;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        registerEvents(new Listener[] {
+                new Oversee()
+        });
+    }
+
+    @Game
+    public void registerEvents(Listener[] listeners) {
+        for (Method method : listeners.getClass().getMethods()) {
+            for (Annotation annotation : method.getAnnotations()) {
+                if (annotation instanceof Game) {
+                    Game ann = (Game) annotation;
+                    if (!ann.isStarted()) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        for (Listener listener : listeners) {
+            getServer().getPluginManager().registerEvents(listener, this);
+        }
+    }
+
+}
