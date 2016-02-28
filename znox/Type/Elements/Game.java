2cd639dabb741136a79a1fa84939f7ad58fee036
zNoX_ <tybeatscode@gmail.com>
2016-02-28 11:51:07 -0300

    Create Game.java

---
+++ znox/Type/Elements/Game.java
@@ -0,0 +1,22 @@
+package znox.Type.Elements;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+/**
+ * <h1>Game Annotation Class</h1>
+ * <h2>Class for declare events for game</h2>
+ *
+ * @author zNoX_
+ * @version 1.0.1
+ */
+public @interface Game {
+
+    boolean isStarted() default false;
+
+}
